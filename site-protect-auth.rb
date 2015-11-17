86a7b7a8a0a47f764355c95f5073e255c8b4993a
Alexey Antipov <alexey.antipov@erlyvideo.org>
2015-11-17 10:04:42 +0300

    Add time interval checking

--- site-protect-auth.rb
+++ site-protect-auth.rb
@@ -15,11 +15,28 @@ end
 
 
 get '/' do
-  print_params params
-  p [SECRET, params["name"], params["ip"]]
-  token = Digest::SHA1.hexdigest [SECRET, params["name"], params["ip"]] . join('|')
-  if token != params["token"]
-    halt 401
+  ts = params["token"] . split('.')
+  hash = ts.shift
+
+  tparams = {}
+  ts . each do |s|
+    s =~ /^([a-z]+)=(.+)$/ or next
+    tparams[$1] = $2
   end
-  "OK"
+
+  time = tparams["time"] . to_i
+  timespan = tparams["timespan"] . to_i
+
+  hashs = [SECRET, params["name"], params["ip"], time, timespan]
+  hash2 = Digest::SHA1.hexdigest hashs . join
+  if hash2 != hash
+    halt 401, "fail"
+  end
+
+  now = Time.now.to_i
+  if now < time || now > (time+timespan)
+    halt 401, "not in time"
+  end
+
+  "ok"
 end
--- site-protect.rb
+++ site-protect.rb
@@ -16,14 +16,17 @@ end
 
 
 get '/:stream' do
-  token = Digest::SHA1.hexdigest [SECRET, params["stream"], request.ip] . join('|')
+  time = Time.now.to_i
+  hashs = [SECRET, params["stream"], request.ip, time.to_s, TIMESPAN]
+  hash = Digest::SHA1.hexdigest hashs . join
+  token = [hash, "time=#{time}", "timespan=#{TIMESPAN}"] . join('.')
   @embed = "#{FLUSSONIC_URL}:8080/#{params["stream"]}/embed.html?dvr=false&token=#{token}"
-  erb :pl
+  erb :player
 end
 
 
-# Retrieve all static streams from FLussonic.
-# This may me done manual instead.
+# Retrieve all static streams from Flussonic.
+# This may me done manually instead.
 def get_streams
   url = "#{FLUSSONIC_URL}/flussonic/api/get_config"
   config = get_json url, FLUSSONIC_USER, FLUSSONIC_PASS
@@ -62,23 +65,17 @@ __END__
 
 @@index
 <html>
-
-<ul>
-<% @streams . each do |stream, name| %>
-
-  <li>
-    <a href="<%= "#{stream}?name=#{name}" %>"> <%= name %> </a>
-  </li>
-  
-<% end %>
-</ul>
-
-
+  <ul>
+  <% @streams . each do |stream, name| %>
+    <li>
+      <a href="<%= "#{stream}?name=#{name}" %>"> <%= name %> </a>
+    </li>
+  <% end %>
+  </ul>
 </html>
 
 
-@@pl
-
+@@player
 <html>
 <%= params["name"] %>
 <br><br>
